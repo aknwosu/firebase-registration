e2e8e7218c63e1236cce47635cb051f2645960a1
Rebecca Nwosu <akunnarebecca@gmail.com>
2020-01-30 16:14:13 +0100

    setup Redux

---
+++ src/reducers/login.js
@@ -0,0 +1,13 @@
+// import { ADD_TODO, TOGGLE_TODO } from "../actionTypes";
+
+const initialState = {
+  isLoggedIn: false,
+  user: {}
+};
+
+export default function (state = initialState, action) {
+  switch (action.type) {
+    default:
+      return state;
+  }
+}
